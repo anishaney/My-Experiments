da143292a8fd1ce7770360eb2c551ac0bed33c10
anishaney <aneyanish@gmail.com>
2013-11-14 22:40:54 -0500

    changes to DeleteLLDuplicate

--- atoi/atoi/LeetCode/DeleteLinkedListDuplicates.cpp
+++ atoi/atoi/LeetCode/DeleteLinkedListDuplicates.cpp
@@ -1,3 +1,8 @@
+/*Algorithm: Keep three pointers to find the duplicates (nodes having same val value). Traverse through the linked list and keep checking the value of the current node->val if equal to the node.next->val. If so, remove the node.next and jump to the subsequent node. If not equal then change the first node to the node.next and subsequent node.*/
+
+/*Complexity: O(n)*/
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -11,20 +16,23 @@ public:
     ListNode *deleteDuplicates(ListNode *head) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
+
+	//Check if Linked List is NULL
         if(head == NULL)
             return head;
         
         ListNode *curr = head, *rmve, *p = curr->next;
+
         while (curr != NULL && p != NULL){
-            if (curr->val == p->val){
-                rmve = p;
-                p = p->next;
-                curr->next = p;
-                free(rmve);
+            if (curr->val == p->val){ //If equal
+                rmve = p; //put current node to a removal pointer
+                p = p->next;//point to the next node.
+                curr->next = p;//join current node to the next node
+                free(rmve);//free the memory allocated to the duplicate node.
             }
-            else{
-                curr = p;
-                p = curr->next;
+            else{//If not equal
+                curr = p; //change current node to point to the next node
+                p = curr->next;//and subsequent node
             }
         }
         return head;
